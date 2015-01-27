1f8605e828286a3299e34bd580cc102926e14c77
Austin Schneider <hogenshpogen@gmail.com>
2015-01-27 00:25:19 -0600

    Added comments. Renamed functions.

--- HW1/H3/float_precision.cc
+++ HW1/H3/float_precision.cc
@@ -2,17 +2,17 @@
 #include <fstream>
 #include <iostream>
 
-template <class T>
+template <class T> // 1/n
 T func_0(int n) {
-	return (T) (1.0/n);
+	return (T) (1.0 / n);
 }
 
-template <class T>
+template <class T> // Taylor series terms for ln(2), alternating series of 1/n
 T func_1(int n) {
-	return ((T)std::pow(-1,n+1))/((T)n);
+	return ((T) std::pow(-1, n + 1)) / ((T) n);
 }
 
-template <class T>
+template <class T> // Sum series from n=1 to n=10^power
 T s_up(int power, T(*func)(int) = func_0) {
 	int max = std::pow(10, power);
 	T sum = 0;
@@ -22,7 +22,7 @@ T s_up(int power, T(*func)(int) = func_0) {
 	return sum;
 }
 
-template <class T>
+template <class T> // Sum series from n=10^power to n=1
 T s_down(int power, T(*func)(int) = func_0) {
 	int max = std::pow(10, power);
 	T sum = 0;
@@ -32,17 +32,17 @@ T s_down(int power, T(*func)(int) = func_0) {
 	return sum;
 }
 
-template <class T, class U>
-void do_diff(T(*f_0)(int,T(*)(int)), U(*f_1)(int,U(*)(int)), T(*ff_0)(int), U(*ff_1)(int), int min, int max, std::ostream & out) {
+template <class T, class U> // Print difference between two functions for a range [min, max]
+void print_diff(T(*f_0)(int, T(*)(int)), U(*f_1)(int, U(*)(int)), T(*ff_0)(int), U(*ff_1)(int), int min, int max, std::ostream & out) {
 	out << "# power\tdiff" << std::endl;
 	for(int power = min; power <= max; ++power) {
 		out << power << "\t" << std::abs(f_0(power, ff_0) - f_1(power, ff_1)) << std::endl;
 	}
 }
 
-template <class T>
+template <class T> // Test the precision of s_up vs s_down
 void test_precision(std::ostream & out) {
-	do_diff(s_up<T>, s_down<T>, func_0<T>, func_0<T>, 2, 8, out);
+	print_diff(s_up<T>, s_down<T>, func_0<T>, func_0<T>, 2, 8, out);
 }
 
 template <class T>
@@ -51,21 +51,21 @@ T ln2(int power, T(*func)(int) = func_0) {
 }
 
 int main() {
-	std::fstream float_file("./float_precision_data.dat", std::ios_base::out);
-	test_precision<float>(float_file);
+	std::fstream single_file("./single_precision_data.dat", std::ios_base::out);
+	test_precision<float>(single_file); // Test precision of float
 	
 	std::fstream double_file("./double_precision_data.dat", std::ios_base::out);
-	test_precision<double>(double_file);
+	test_precision<double>(double_file); // Test precision of double
 	
 	std::fstream sup_err_file("./single_sup_err.dat", std::ios_base::out);
-	do_diff(s_up<float>, s_up<double>, func_0, func_0, 2, 8, sup_err_file);
+	print_diff(s_up<float>, s_up<double>, func_0, func_0, 2, 8, sup_err_file); // Test s_down<float> vs s_down<double>
 	
 	std::fstream sdown_err_file("./single_sdown_err.dat", std::ios_base::out);
-	do_diff(s_down<float>, s_up<double>, func_0, func_0, 2, 8, sdown_err_file);
+	print_diff(s_down<float>, s_up<double>, func_0, func_0, 2, 8, sdown_err_file); // Test s_up<float> vs s_down<double>
 	
 	std::fstream alter_single_err_file("./alter_single_sup_err.dat", std::ios_base::out);
-	do_diff(s_up<float>, ln2<double>, func_0, func_0, 2, 9, alter_single_err_file);
+	print_diff(s_up<float>, ln2<double>, func_0, func_0, 2, 9, alter_single_err_file); // Test s_up<float> vs library function for ln(2)
 	
 	std::fstream alter_double_err_file("./alter_double_sup_err.dat", std::ios_base::out);
-	do_diff(s_up<double>, ln2<double>, func_1, func_1, 2, 9, alter_double_err_file);
+	print_diff(s_up<double>, ln2<double>, func_1, func_1, 2, 9, alter_double_err_file); // Test s_up<double> vs library function for ln(2)
 }
