970d5435c8cbba3db16479831a9169fac2bea7d3
Austin Schneider <hogenshpogen@gmail.com>
2015-01-26 22:57:40 -0600

    Made sums more general. Moved hard-coded ranges. Added last part of question.

--- HW1/H3/float_precision.cc
+++ HW1/H3/float_precision.cc
@@ -3,36 +3,51 @@
 #include <iostream>
 
 template <class T>
-T s_up(int power) {
+T func_0(int n) {
+	return (T) (1.0/n);
+}
+
+template <class T>
+T func_1(int n) {
+	return ((T)std::pow(-1,n+1))/((T)n);
+}
+
+template <class T>
+T s_up(int power, T(*func)(int) = func_0) {
 	int max = std::pow(10, power);
 	T sum = 0;
 	for(int n = 1; n <= max; ++n) {
-		sum += (T) n;
+		sum += func(n);
 	}
 	return sum;
 }
 
 template <class T>
-T s_down(int power) {
+T s_down(int power, T(*func)(int) = func_0) {
 	int max = std::pow(10, power);
 	T sum = 0;
 	for(int n = max; n >= 1; --n) {
-		sum += (T) n;
+		sum += func(n);
 	}
 	return sum;
 }
 
 template <class T, class U>
-void do_diff(T(*f_0)(int), U(*f_1)(int), std::ostream & out) {
+void do_diff(T(*f_0)(int,T(*)(int)), U(*f_1)(int,U(*)(int)), T(*ff_0)(int), U(*ff_1)(int), int min, int max, std::ostream & out) {
 	out << "# power\tdiff" << std::endl;
-	for(int power = 2; power <= 8; ++power) {
-		out << power << "\t" << std::abs(f_0(power) - f_1(power)) << std::endl;
+	for(int power = min; power <= max; ++power) {
+		out << power << "\t" << std::abs(f_0(power, ff_0) - f_1(power, ff_1)) << std::endl;
 	}
 }
 
 template <class T>
 void test_precision(std::ostream & out) {
-	do_diff(s_up<T>, s_down<T>, out);
+	do_diff(s_up<T>, s_down<T>, func_0<T>, func_0<T>, 2, 8, out);
+}
+
+template <class T>
+T ln2(int power, T(*func)(int) = func_0) {
+	return (T) std::log(2);
 }
 
 int main() {
@@ -43,8 +58,14 @@ int main() {
 	test_precision<double>(double_file);
 	
 	std::fstream sup_err_file("./single_sup_err.dat", std::ios_base::out);
-	do_diff(s_up<float>, s_up<double>, sup_err_file);
+	do_diff(s_up<float>, s_up<double>, func_0, func_0, 2, 8, sup_err_file);
 	
 	std::fstream sdown_err_file("./single_sdown_err.dat", std::ios_base::out);
-	do_diff(s_down<float>, s_up<double>, sdown_err_file);
+	do_diff(s_down<float>, s_up<double>, func_0, func_0, 2, 8, sdown_err_file);
+	
+	std::fstream alter_single_err_file("./alter_single_sup_err.dat", std::ios_base::out);
+	do_diff(s_up<float>, ln2<double>, func_0, func_0, 2, 9, alter_single_err_file);
+	
+	std::fstream alter_double_err_file("./alter_double_sup_err.dat", std::ios_base::out);
+	do_diff(s_up<double>, ln2<double>, func_1, func_1, 2, 9, alter_double_err_file);
 }
